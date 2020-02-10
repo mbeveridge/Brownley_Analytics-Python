772bdf294eb1849560c0c23b7d7d681e62450c5f
Mark Beveridge <mark@dreamtime.org>
2020-02-10 16:43:52 +0000

    first_script.py (add 2 more examples P.8)

--- first_script.py
+++ first_script.py
@@ -7,3 +7,17 @@ Created on Mon Feb 10 15:45:49 2020
 """
 
 print("Output #1: I'm excited to learn Python.")
+
+
+# Add two numbers together
+x=4
+y=5
+z=x+y
+print("Output #2: Four plus five equals {0:d}.".format(z))
+
+
+# Add two lists together
+a=[1,2,3,4]
+b = ["first", "second", "third", "fourth"]
+c=a+b
+print("Output #3: {0}, {1}, {2}".format(a, b, c))
