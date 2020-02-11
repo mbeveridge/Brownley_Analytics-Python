0aa159174a456d747216ddd722528702bb315bd2
Mark Beveridge <mark@dreamtime.org>
2020-02-11 17:27:37 +0000

    first_Script.py (output #'s 18-20, P.15)

--- first_script.py
+++ first_script.py
@@ -47,7 +47,7 @@ print("Output #12: {0:.2f}".format(log(4)))
 print("Output #13: {0:.1f}".format(sqrt(81)))
 
 
-# Strings ...P.14
+# Strings ...Page14
 print("Output #14: {0:s}".format('I\'m enjoying learning Python.'))
 print("Output #15: {0:s}".format("This is a long string. Without the backslash\
  it would run off of the page on the right in the text editor and be very\
@@ -58,3 +58,14 @@ print("Output #16: {0:s}".format('''You can use triple single quotes
 for multi-line comment strings.'''))
 print("Output #17: {0:s}".format("""You can also use triple double quotes
 for multi-line comment strings."""))
+
+
+# operators on strings ...Page15
+string1 = "This is a "
+string2 = "short string."
+sentence = string1 + string2
+print("Output #18: {0:s}".format(sentence))
+print("Output #19: {0:s} {1:s}{2:s}".format("She is", "very "*4, "beautiful."))
+m = len(sentence)
+print("Output #20: {0:d}".format(m))
+
