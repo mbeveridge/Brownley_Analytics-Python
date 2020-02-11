1564479fc4f62986e8f0beb0fb2ea41dfba42fad
Mark Beveridge <mark@dreamtime.org>
2020-02-11 17:51:58 +0000

    first_script.py (output #'s 21-24, P.16)

--- first_script.py
+++ first_script.py
@@ -69,3 +69,18 @@ print("Output #19: {0:s} {1:s}{2:s}".format("She is", "very "*4, "beautiful."))
 m = len(sentence)
 print("Output #20: {0:d}".format(m))
 
+
+# `split` function ...Page16
+string1 = "My deliverable is due in May"
+string1_list1 = string1.split()
+string1_list2 = string1.split(" ",2)
+print("Output #21: {0}".format(string1_list1))
+print("Output #22: FIRST PIECE:{0} SECOND PIECE:{1} THIRD PIECE:{2}"\
+.format(string1_list2[0], string1_list2[1], string1_list2[2]))
+string2 = "Your,deliverable,is,due,in,June"
+string2_list = string2.split(',')
+print("Output #23: {0}".format(string2_list))
+print("Output #24: {0} {1} {2}".format(string2_list[1], string2_list[5],\
+string2_list[-1]))
+
+
