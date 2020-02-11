196850bf9f9dedb9bf45c976e31dba8cbdab5799
Mark Beveridge <mark@dreamtime.org>
2020-02-11 16:46:31 +0000

    first_script.py (output #'s 4-10, P.12)

--- first_script.py
+++ first_script.py
@@ -21,3 +21,19 @@ a=[1,2,3,4]
 b = ["first", "second", "third", "fourth"]
 c=a+b
 print("Output #3: {0}, {1}, {2}".format(a, b, c))
+
+
+# Integers
+x=9
+print("Output #4: {0}".format(x))
+print("Output #5: {0}".format(3**4))
+print("Output #6: {0}".format(int(8.3)/int(2.7)))
+
+
+# Floating-point numbers
+print("Output #7: {0:.3f}".format(8.3/2.7))
+y = 2.5*4.8
+print("Output #8: {0:.1f}".format(y))
+r = 8/float(3)
+print("Output #9: {0:.2f}".format(r))
+print("Output #10: {0:.4f}".format(8.0/3))
