1dd84efe987d81662d727d4ed99c27e9c4090d2c
Mark Beveridge <mark@dreamtime.org>
2020-02-13 15:47:26 +0000

    first_script.py (output #'s 58-72, P.26)

--- first_script.py
+++ first_script.py
@@ -203,6 +203,39 @@ print("Output #56: {!s}".format(datetime.date(datetime.strptime\
 print("Output #57: {!s}".format(datetime.date(datetime.strptime\
                                               (date4, '%B %d, %Y'))))
 
+
+
+
+# Use square brackets to create a list ...Page26
+# `len()` counts the number of elements in a list
+# `max()` and `min()` find the maximum and minimum values
+# `count()` counts the number of times a value appears in a list
+a_list = [1, 2, 3]
+print("Output #58: {}".format(a_list))
+print("Output #59: a_list has {} elements.".format(len(a_list)))
+print("Output #60: the maximum value in a_list is {}.".format(max(a_list)))
+print("Output #61: the minimum value in a_list is {}.".format(min(a_list)))
+another_list = ['printer', 5, ['star', 'circle', 9]]
+print("Output #62: {}".format(another_list))
+print("Output #63: another_list also has {} elements.".format\
+      (len(another_list)))
+print("Output #64: 5 is in another_list {} time.".format(another_list.count(5)))
+
+
+# Use index values to access specific elements in a list ...Page26
+# [0] is the first element; [-1] is the last element
+print("Output #65: {}".format(a_list[0]))
+print("Output #66: {}".format(a_list[1]))
+print("Output #67: {}".format(a_list[2]))
+print("Output #68: {}".format(a_list[-1]))
+print("Output #69: {}".format(a_list[-2]))
+print("Output #70: {}".format(a_list[-3]))
+print("Output #71: {}".format(another_list[2]))
+print("Output #72: {}".format(another_list[-1]))
+    
+    
+    
+
                                 
                               
                                 
